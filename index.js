676b8aa6ed0cda65c677931711d4b4f226590318
aishanirfan <aishanirfan6@gmail.com>
2024-06-18 17:51:01 +0530

    added some get routes

--- index.js
+++ index.js
@@ -2,18 +2,35 @@ const express = require('express')
 
 const app = express()
 
+const courses = [
+    {
+        id: 1,
+        name : "Networks"
+    },
+    {
+        id: 2,
+        name : "OS"
+    },
+    {
+        id: 3,
+        name : "DSA"
+    }
+]
+
 
 app.get('/', (req, res)=> {
     res.send("Hello there")
 })
 
 app.get('/api/courses', (req, res)=> {
-    res.send([1, 2, 3])
+    res.send(courses)
 })
 
-// app.get('/api/courses/:id', (req, res)=> {
-
-// })
+app.get('/api/courses/:id', (req, res)=> {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if(!course) res.status(404).send("The course with the given ID was not found!")
+    res.send(course)
+})
 
 
 const port = process.env.PORT || 3000
