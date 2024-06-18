c61698f685e99dd50a66d89749bcdaff8db16172
aishanirfan <aishanirfan6@gmail.com>
2024-06-18 20:28:24 +0530

    added a patch route

--- index.js
+++ index.js
@@ -31,16 +31,12 @@ app.get('/api/courses', (req, res)=> {
 
 app.post('/api/courses', (req, res)=> {
 
-    const schema = {
-        name: Joi.string().min(3).required()
-    }
-
-    const result = Joi.validate(req.body, schema)
-
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message)
+    const {error} = validateCourse(course)
+    if (error) {
+        res.status(400).send(error.details[0].message)
         return;
     }
+    
     const course = {
         id : courses.length+1,
         name: req.body.name
@@ -56,6 +52,29 @@ app.get('/api/courses/:id', (req, res)=> {
     res.send(course)
 })
 
+app.patch('/api/courses/:id', (req, res)=>{
+    const course = courses.find(c=> c.id === parseInt(req.params.id))
+    if(!course) res.status(404).send("The course with the given id was not found!")
+    
+       const {error} = validateCourse(course)
+        if (error) {
+            res.status(400).send(error.details[0].message)
+            return;
+        }
+
+        course.name = req.body.name
+        res.send(course)
+    
+})
+
 
 const port = process.env.PORT || 3000
 app.listen(port, ()=> console.log(`Listening on port ${port}`))
+
+function validateCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    return Joi.validate(course, schema)
+}
